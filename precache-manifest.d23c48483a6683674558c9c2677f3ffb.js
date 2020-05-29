33e82242423c6f6a1bf31f1a6657bf42a4e46dd7
CedomkiviriLinux <andresencmejia@hotmail.com>
2020-05-29 13:50:20 -0400

    Updates

---
+++ precache-manifest.d23c48483a6683674558c9c2677f3ffb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe0dffb8adf82cb50db385afa7e7c4d9",
+    "url": "/TriviaApp/index.html"
+  },
+  {
+    "revision": "4980649309cc7a27d7fb",
+    "url": "/TriviaApp/static/js/2.dc767e39.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TriviaApp/static/js/2.dc767e39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5975bbcefdb6b869750",
+    "url": "/TriviaApp/static/js/main.f70b185b.chunk.js"
+  },
+  {
+    "revision": "b04b20e8a2bbdf3c9874",
+    "url": "/TriviaApp/static/js/runtime-main.e9e88697.js"
+  }
+]);
