3603f640f9015d0c2a85bac48571a78f6beed948
Cesar Encarnacion <cesaraencarnacionm@gmail.com>
2020-06-05 12:44:07 -0400

    added new trivia button

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -145,6 +145,7 @@ class Questions extends React.Component {
 
                 </div>
 
+                <button className="btn btn-secondary " onClick={()=> window.location.reload()} style={{ margin: "30px" }}>New trivia</button>
 
 
             </div>
